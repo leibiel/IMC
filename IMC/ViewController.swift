cc5cc43f94299bac1ee361198e46e3144720564f
Gabriel Campos <emailpragabriel@mail.com>
2018-07-16 10:54:50 -0300

    Realizada programação e cálculos do IMC

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tfWeight: UITextField!
     @IBOutlet weak var tfHeight: UITextField!
     @IBOutlet weak var lbResult: UILabel!
-    @IBOutlet weak var lvResult: UIImageView!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    
+    var imc: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +30,36 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!) {
+            imc = weight / (height*height)
+            showResults()
+        }
     }
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            case 16..<18,5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 18,5..<25:
+                result = "Peso ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        lbResult.text = result
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+        view.endEditing(true)
+    }
+
     
 }
-
